71b75d3179f5f08751473e2b829588f88e4b2071
JulioCRam <julioramirez692@gmail.com>
2025-09-17 18:44:58 -0500

    se agrega funcion para guadar amigos y validar campo vacio

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+let amigo = document.querySelector('#amigo');
+let guardar = document.querySelector('#guardar')
+
+function agregarAmigo(){
+    let nombreAmigos = amigo.value.trim();  
+    if(nombreAmigos !== ''){
+        amigo.value = '';
+        nombres.push(nombreAmigos);
+         console.log(nombres);  
+    }else {
+    alert("este campo no puede estar vacio");
+    }
+};
+
+
+
