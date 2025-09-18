e49b6ccc92a8d7034ee865ae3649963abee7fc70
JulioCRam <julioramirez692@gmail.com>
2025-09-17 19:29:41 -0500

    se agraga lista de amigos

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombres = [];
 let amigo = document.querySelector('#amigo');
-let guardar = document.querySelector('#guardar')
+let guardar = document.querySelector('#guardar');
+let listaAmigos = document.querySelector('#listaAmigos');
+let listaResultado = document.querySelector('#resultado')
 
 function agregarAmigo(){
     let nombreAmigos = amigo.value.trim();  
     if(nombreAmigos !== ''){
         amigo.value = '';
         nombres.push(nombreAmigos);
-         console.log(nombres);  
+         console.log(nombres); 
+             mostrarlista(); 
+
     }else {
     alert("este campo no puede estar vacio");
     }
 };
 
 
+function mostrarlista(){
+    listaResultado.innerHTML = '';
+    listaAmigos.innerHTML = '';
 
+nombres.forEach(nombres => {
+    let li = document.createElement('li');
+    li.textContent = nombres;
+    listaAmigos.appendChild(li);
+})
+}
+
+
+   
